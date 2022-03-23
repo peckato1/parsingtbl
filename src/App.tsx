peckato1/parsingtbl

commit 1d5acec5d60a803f85c3b9f2d6c56af476fac76c

app: comments in the input

src/App.tsx
@@ -26,6 +26,10 @@ function parseGrammar(text: string | undefined) {
 			continue
 		}
 
+		if (row.startsWith("#")) {
+			continue
+		}
+
 		let splitted = row.split("->").map((e) => e.trim().split(" ").filter(e => e.length > 0))
 		if (splitted.length !== 2) {
 			throw new Error("Unable to decompose rule. None or multiple '->' on one line")

src/components/InputForm.tsx
@@ -26,6 +26,7 @@ export function GrammarForm({onChange, error, height, value}: Props) {
 					Symbols must be separated by a space (otherwise they are considered as one symbol).
 					Symbols starting with uppercase letter are considered as nonterminals.
 					Left hand side of the first rule defines the initial symbol.
+					Lines starting with <code>#</code> are comments and do not take part in the computation.
 			</div>
 		</div>
 	   );

commit 6c868de5bc2526c8004a759396f0952b775a66cf

app: fix parsing comments
 - all commented lines means the input is empty
 - hash does not have to be exactly first character on the line, it is
   sufficient for it to be first non-whitespace char

Fixes: 1d5acec5d60a803f85c3b9f2d6c56af476fac76c

src/App.tsx
@@ -21,12 +21,14 @@ function parseGrammar(text: string | undefined) {
 		throw new Error("Empty input")
 	}
 
-	for(let row of text.trim().split("\n")) {
-		if (row.trim().length === 0) {
-			continue
-		}
+	let rows = text.trim().split("\n").filter(row => !row.trim().startsWith("#"))
 
-		if (row.startsWith("#")) {
+	if (rows.length === 0) {
+		throw new Error("Empty input")
+	}
+
+	for(let row of rows) {
+		if (row.trim().length === 0) {
 			continue
 		}
 