e15b9d9223290992c4e9aa55aee41cd7ca5ec1d8
Tomas Pecka <peckato1@fit.cvut.cz>
2022-06-02 11:35:30 +0200

    analyzer: add next operation column

--- src/components/Analyzer.tsx
+++ src/components/Analyzer.tsx
@@ -1,23 +1,53 @@
 import { Grammar, NonterminalSymbol, TerminalSymbol, Epsilon, epsilon, GrammarSymbolOrEpsilon } from '../ll'
 
 const htmlEps = String.fromCharCode(949)
+const htmlArrow = String.fromCharCode(10230)
+
 const show = (inp: GrammarSymbolOrEpsilon[] | Set<GrammarSymbolOrEpsilon>) => {
 	let vals = [...inp]
 	return vals.length > 0 ? vals.map((e) => e === epsilon ? htmlEps : e).join(" ") : [htmlEps]
 }
 
+interface Expansion {
+	type: "expansion"
+	rule: number
+}
+
+interface Match {
+	type: "match"
+	symbol: TerminalSymbol
+}
+
+const showOp = (inp: Expansion | Match | undefined, grammar: Grammar) => {
+	if (inp === undefined) return ""
+
+	switch(inp.type) {
+		case "expansion":
+			const op = inp as Expansion
+			return (
+				<span>
+					expansion {op.rule + 1} ( <code>{show([grammar.rules[op.rule].lhs])}</code> {htmlArrow} <code>{show( grammar.rules[op.rule].rhs )}</code> )
+				</span>
+			)
+		case "match":
+			return ( <span>match <code>{inp.symbol}</code></span> )
+	}
+}
+
 interface Step {
-	input: TerminalSymbol[];
-	stack: GrammarSymbolOrEpsilon[];
-	parse: number[];
+	input: TerminalSymbol[]
+	stack: GrammarSymbolOrEpsilon[]
+	parse: number[]
+	nextOp: Expansion | Match | undefined
 }
 
 function parse(grammar: Grammar, parseTable: Map<NonterminalSymbol, Map<TerminalSymbol|Epsilon, Set<number>>>, word: TerminalSymbol[]) : Step[] {
 	let input = [...word]
 	let stack = [grammar.initialSymbol]
 	let parse: number[] = []
+	let operation = undefined
 
-	let res: Step[] = [{input: [...input], stack: [...stack], parse: [...parse]}]
+	let res: Step[] = [{input: [...input], stack: [...stack], parse: [...parse], nextOp: operation}]
 
 	while (stack.length > 0) {
 		let last = stack[0]
@@ -33,14 +63,17 @@ function parse(grammar: Grammar, parseTable: Map<NonterminalSymbol, Map<Terminal
 			stack = [...grammar.rules[rule].rhs].concat(stack)
 
 			parse.push(rule)
+			operation = { type: "expansion" as const, rule: rule }
 		} else if (input.length > 0 && last && last === input[0]) { // match
+			operation = { type: "match" as const, symbol: input[0] }
 			stack.shift()
 			input.shift()
 		} else {
 			break
 		}
 
-		res.push({input: [...input], stack: [...stack], parse: [...parse]})
+		res[res.length - 1].nextOp = operation
+		res.push({input: [...input], stack: [...stack], parse: [...parse], nextOp: undefined })
 	}
 	return res
 }
@@ -60,11 +93,12 @@ export default function Analyzer({grammar, parseTable, word}: Props) {
 					<th>Input</th>
 					<th>Stack</th>
 					<th>Left parse</th>
+					<th>Next operation</th>
 				</tr>
 			</thead>
 			<tbody>
 				{parse(grammar, parseTable, word).map((step, i, array) => {
-					const {input, stack, parse} = step
+					const {input, stack, parse, nextOp} = step
 					const className = (i === array.length - 1) ? (stack.length === 0 && input.length === 0 ? "table-success" : "table-danger") : ""
 					return (
 						<tr className={className}>
@@ -72,6 +106,7 @@ export default function Analyzer({grammar, parseTable, word}: Props) {
 							<td><code>{show(input)}</code></td>
 							<td><code>{show(stack)}</code></td>
 							<td>{parse.map(i => i + 1).join(", ")}</td>
+							<td>{showOp(nextOp, grammar)}</td>
 						</tr>
 					)
 				})}
